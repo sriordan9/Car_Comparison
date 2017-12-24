a1df6654a0c31fbbb2f6643a2b2f3cc723deda33
Sean Riordan <riordan.sean9@gmail.com>
2017-12-23 23:12:59 -0500

    Completed make, model, and trim dropdown functions

--- scripts/script.js
+++ scripts/script.js
@@ -75,7 +75,8 @@ class View {
     arrayToList(array) {
         let valuesArray = array.map(keyValPair => 
             `<option>${keyValPair.make} ${keyValPair.model} ${keyValPair.type}</option>`);            
-        return `<option selected="selected" disabled="disabled">Select model</option> ${valuesArray.join('')}`;
+        return `<option selected="selected" disabled="disabled">Select model</option>
+            ${valuesArray.join('')}`;
     }
     // Updates UI with model options in dropdown menu
     dropDownList(optionList) {
@@ -86,11 +87,9 @@ class View {
     // For Car API-------------------------->
     // Changes arrays into simple list of html options for API
     arrayToListApi(array) {
-        console.log(array);
-        console.log("Array length: " + array.length);
-        let valuesArray = array.map(year => `<option>${year}</option>`);
-        console.log(valuesArray);         
-        return `<option selected="selected" disabled="disabled">Select year</option>${valuesArray.join('')}`;
+        let valuesArray = array.map(component => `<option>${component}</option>`);        
+        return `<option selected="selected" disabled="disabled">Please Select</option>
+            ${valuesArray.join('')}`;
     }
     // Updates UI with years options
     dropDownListApi(element, optionList) {
@@ -112,6 +111,9 @@ class Controller {
         this.ajaxRequest = new XMLHttpRequest();
         this.updateDropdowns;
         this.pullApiData();
+        this.userYearInput;
+        this.userMakeInput;
+        this.userModelInput;
     }
     // Event handler when user clicks done button after filling out make/model fields
     doneButton() {
@@ -121,7 +123,8 @@ class Controller {
 
             this.projectModel.modelSelection(this.userInputMakes, this.userInputModels);
 
-            this.projectView.dropDownList(this.projectView.arrayToList(this.projectModel.listInputModels()));
+            this.projectView.dropDownList(this.projectView.arrayToList(
+                this.projectModel.listInputModels()));
         });
     } 
     // Event handler when user presses enter key after filling out make/model fields
@@ -132,73 +135,78 @@ class Controller {
     
             if(event.keyCode === 13 || event.which === 13) {
                 this.projectModel.modelSelection(this.userInputMakes, this.userInputModels);
-                this.projectView.dropDownList(this.projectView.arrayToList(this.projectModel.listInputModels()));
+                this.projectView.dropDownList(this.projectView.arrayToList(
+                    this.projectModel.listInputModels()));
             } 
         });
     } 
-    // Pull only US makes
-    // carMakes () {
-    //     let link = 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getMakes&year=2000&sold_in_us=1';
-    //     // let selectedYear = user input and place this variable in line below as value for 'year'
-    //     $.getJSON(this.base_url = `${link}?callback=?`, {cmd:"getMakes", year:"2009"},
-    //     function(data) {
-    //         const makes = data.Makes;
-    //         for (var i = 0; i < makes.length; i++) {
-    //             console.log(makes[i].make_display);
-                
-                
-    //         }
-            
-    //     });
-    // }
     // Calls two functions from the View to wrap array components in <option> tags, then 
-    // places those new elements into the index.html file. Takes two parameters: the <select> element 
-    // where the info will be placed and the data to be placed.
+    // places those new elements into the index.html file. Takes two parameters: the attribute 
+    //  selector of where the info will be placed and the data to be placed.
     updateDropdowns(element, dropdownData) {
         this.projectView.dropDownListApi(element, this.projectView.arrayToListApi(dropdownData));
     }
 
     pullApiData () {
         // Links to access car API data
-        let link = {years: 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getYears',
-                    makes: 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getMakes&year=2000&sold_in_us=1',
-                    models: 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getModels&make=ford&year=2005&sold_in_us=1&body=SUV',
-                    trims: 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getTrims&[params'};
+        let link = 'https://www.carqueryapi.com/api/0.3/';
         // Pull data of years available            
-        // let selectedYear = user input and place this variable in line below as value for 'year'
-        $.getJSON(this.base_url = `${link.years}?callback=?`, {cmd:"getYears"},
-        (data) => {
+        $.getJSON(this.base_url = `${link}?callback=?`, {cmd:"getYears"},(data) => {
             const years = data.Years;
             const yearsArray = [];
             // API only has min and max year, this counts out all years in between
             for (var i = Number(years.min_year); i <= years.max_year; i++) {
-            yearsArray.push(i);
-                            
+                yearsArray.push(i);            
             }
-            console.log('completed first request');
-            this.updateDropdowns(`select[name="years"]`, yearsArray);
-            console.log("moved past first request");
+            this.updateDropdowns(`select[name="years"]`, yearsArray);            
         });
-        // console.log("moved past first request");
-        // document.querySelector(`select[name="years"]`).addEventListener('onchange', () => {
-        //     console.log("I'm in");
-        //     let userYearInput = document.querySelector(`select[name="years"]`).value;
-        //     console.log(userYearInput);
-        //     // let selectedYear = user input and place this variable in line below as value for 'year'
-        //     $.getJSON(this.base_url = `${link.makes}?callback=?`, {cmd:"getMakes", year: userYearInput},
-        //     (data) => {
-        //         const makes = data.Makes;
-        //         const makesArray = []
-        //         for (var i = 0; i < makes.length; i++) {
-        //             // console.log(makes[i].make_display);
-        //             makesArray.push(makes[i].make_display);
-        //         }
-        //         console.log(makesArray);
-                
-        //         this.updateDropdowns(`select[name="makes"]`, makesArray);
-                
-        //     });
-        // });
+        // Listens for a year to be selected, once a change has been made for the year dropdown
+        // then a request is sent for the makes data based on the year the user chose
+        document.querySelector(`select[name="years"]`).addEventListener('change', () => {
+            this.userYearInput = document.querySelector(`select[name="years"]`).value;
+
+            $.getJSON(this.base_url = `${link}?callback=?`, 
+                {cmd:"getMakes", year: this.userYearInput, sold_in_us: "1"},(data) => {
+                    const makes = data.Makes;
+                    const makesArray = []
+                    for (var i = 0; i < makes.length; i++) {
+                        makesArray.push(makes[i].make_display);
+                    }
+                    this.updateDropdowns(`select[name="makes"]`, makesArray);
+            });
+        });
+        // Listens for a make to be selected, once a change has been made for the car make dropdown
+        // then a request is sent for the models data based on the make the user chose
+        document.querySelector(`select[name="makes"]`).addEventListener('change', () => {
+            this.userMakeInput = document.querySelector(`select[name="makes"]`).value;
+
+            $.getJSON(this.base_url = `${link}?callback=?`, 
+                {cmd:"getModels", make: this.userMakeInput, year: this.userYearInput, sold_in_us: "1"},
+                    (data) => {
+                        const models = data.Models;
+                        const modelsArray = []
+                        for (var i = 0; i < models.length; i++) {
+                            modelsArray.push(models[i].model_name);
+                        }
+                        this.updateDropdowns(`select[name="models"]`, modelsArray);
+            });
+        });
+        // Listens for a model to be selected, once a change has been made for the car model dropdown
+        // then a request is sent for the trims data based on the model the user chose
+        document.querySelector(`select[name="models"]`).addEventListener('change', () => {
+            this.userModelInput = document.querySelector(`select[name="models"]`).value;
+
+            $.getJSON(this.base_url = `${link}?callback=?`, 
+                {cmd:"getTrims", model: this.userModelInput, make: this.userMakeInput,
+                    year: this.userYearInput, sold_in_us: "1"},(data) => {
+                        const trims = data.Trims;
+                        const trimsArray = []
+                        for (var i = 0; i < trims.length; i++) {
+                            trimsArray.push(trims[i].model_trim);
+                        }
+                        this.updateDropdowns(`select[name="trims"]`, trimsArray);
+            });
+        });
     }
 }
 
@@ -209,8 +217,8 @@ function startup() {
 window.onload = startup;
 
 // GAMEPLAN
-    // 1) Remove case sensitivity of car arrays
-    // 2) specs from local car list should be entered in output fields
-    // 3) use arrow functions to add eventlisteners to each section
+    // 1) Use event delegation in order to use functions for target section (use form.querySelector?).
+    // 2) Input specs of chosen trim
+    // 3) Use arrow functions to add eventlisteners to each section
 
     // Makes page: https://www.autoevolution.com/cars/
